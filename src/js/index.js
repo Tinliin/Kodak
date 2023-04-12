e17c8749e79d9ba841ba74fb837df0d677dc84d9
Александр Заяц <zayatc.av@gmail.com>
2023-04-12 17:40:18 +0300

    made contacts page, fixed blocks animation

--- src/js/index.js
+++ src/js/index.js
@@ -23,3 +23,6 @@ AOS.init({
     anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
   
   });
+
+let header = document.getElementsByTagName("header");
+console.log(header);
